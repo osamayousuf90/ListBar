1d75fec999bf7ce6a9e1b42a5ac3ef6b4f9c1885
Linkitsoft <Linkitsoftdeveloper@gmail.com>
2023-01-17 11:45:21 +0500

    nothing

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
+import React, { useState, useRef } from 'react';
 import obj from "../API";
 import { useNavigate } from "react-router-dom"
 
 const Home = () => {
   // const [searchVal, setSearchVal] = useState("");
   // const [arr, setArr] = useState([obj])
+  const [inputValue, setInputValue] = useState('');
+  const inputRef = useRef(null);  
   
   // const navigate = useNavigate();
     
@@ -14,6 +15,35 @@ const Home = () => {
     //   const s = searchVal ? arr.filter((res) => res?.name.toLowerCase().includes(searchVal.toLowerCase())) : data;
     //   setArr(s)
     // }, [searchVal])
+  
+    function handleChange(event) {
+      const value = event.target.value;
+      if (value.length > 3) {
+        setInputValue(parseFloat(value).toFixed(2));
+      } else {
+        setInputValue(value);
+      }
+  }
+  
+
+  function handleKeyDown(event) {
+    if (event.key === 'Backspace') {
+      const words = inputRef.current.value.split(' ');
+      words.pop();
+      setInputValue(words.join(' '));
+    }
+  }
+
+
+ const isDecimalKey = (e) => {
+   if (e.charCode == 46 || e.charCode === 0 || /\d/.test(String.fromCharCode(e.charCode))) {
+      console.log("is number");
+    	return true
+   } else {
+     console.log("not number")
+    	e.preventDefault();
+    }
+}
   return (
     <div>
       {/* <div className="container main">
@@ -48,7 +78,16 @@ const Home = () => {
         <div className="grid-container_item"> <h5>Hello This is Porter</h5> <img src="https://statinfer.com/wp-content/uploads/dummy-user.png"/> </div>
         <div className="grid-container_item"> <h5>Hello This is Porter</h5> <img src="https://statinfer.com/wp-content/uploads/dummy-user.png"/> </div>
         <div className="grid-container_item"> <h5>Hello This is Porter</h5> <img src="https://statinfer.com/wp-content/uploads/dummy-user.png"/> </div>
-        <div className="grid-container_item"> <h5>Hello This is Porter</h5> <img src="https://statinfer.com/wp-content/uploads/dummy-user.png"/> </div> 
+        <div className="grid-container_item"> <h5>Hello This is Porter</h5> <img src="https://statinfer.com/wp-content/uploads/dummy-user.png" /> </div> 
+        
+        <input
+        required name="balance" min="0" value={inputValue} step="any"
+        ref={inputRef}
+        type="text"
+        // onChange={(event) => handleChange(event)}
+        onChange={(e) => isDecimalKey(e)}
+        placeholder='Place Values' />
+        
       </div>
      </div>
   )
