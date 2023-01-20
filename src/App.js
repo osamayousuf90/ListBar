faf051ebaa89f278221e086bae413aa960d520bf
Linkitsoft <Linkitsoftdeveloper@gmail.com>
2023-01-20 14:57:01 +0500

    custom pagination added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import { BrowserRouter , Routes , Route } from 'react-router-dom';
 import Home from './Page/Home';
 import Profile from './Page/Profile';
+import CustomPagination from './CustomPagination/CustomPagination';
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />}></Route>
+          <Route path='/' element={<CustomPagination />}></Route>
           <Route path="/profile" element={<Profile/>}></Route>
         </Routes>
       </BrowserRouter>
